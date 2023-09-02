c5aa276ee6fdb3ab758b30061800041b38190cf8
vlad <wewef@mail.ru>
2023-09-02 16:15:51 +0200

    add Tack1

---
+++ src/main/java/de/telran/lesson2/Tack2.java
@@ -0,0 +1,20 @@
+package de.telran.lesson2;
+
+public class Tack2 {
+    public static void main(String[] args) {
+        char a1 = 'G';
+        int a2 = 89;
+        byte a3 = 4;
+        short a4 = 56;
+        float a5 = 4.7333436f;
+        double a6 = 4.355453532;
+        long a7 = 12121;
+        System.out.println("char:" + a1);
+        System.out.println("int:" + a2);
+        System.out.println("byte:" + a3);
+        System.out.println("short:"+a4);
+        System.out.println("float:"+a5);
+        System.out.println("double:"+a6);
+        System.out.println("long:"+a7);
+    }
+}
